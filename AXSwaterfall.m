7363dcba6e7a272c98e85f45e070297394598b68
Oli Balmford <oliver.balmford@gmail.com>
2018-03-22 16:03:50 +0000

    Clean-up AXSwaterfall comments and dirty code

--- AXSwaterfall.m
+++ AXSwaterfall.m
@@ -42,71 +42,29 @@ fig.Color = 'w';
 
 w = waterfall(q,temp,results.');
 
-% % Custom color map
-% map = [
-% 0.035294118,0,0.839215686
-% 0,0.243137255,0.823529412
-% 0,0.509803922,0.807843137
-% 0,0.768627451,0.792156863
-% 0,0.780392157,0.541176471
-% 0,0.764705882,0.274509804
-% 0,0.749019608,0.015686275
-% 0.231372549,0.733333333,0
-% 0.470588235,0.717647059,0
-% 0.698039216,0.705882353,0
-% 0.690196078,0.462745098,0
-% 0.674509804,0.22745098,0
-% 0.729411765,0,0
-% 0.729411765,0,0
-% 0.674509804,0.22745098,0
-% 0.690196078,0.462745098,0
-% 0.698039216,0.705882353,0
-% 0.470588235,0.717647059,0
-% 0.231372549,0.733333333,0
-% 0,0.749019608,0.015686275
-% 0,0.764705882,0.274509804
-% 0,0.780392157,0.541176471
-% 0,0.768627451,0.792156863
-% 0,0.509803922,0.807843137
-% 0,0.243137255,0.823529412
-% 0.035294118,0,0.839215686
-% ];
-
-% % Setting colors along the Y axis
-% colormap(map);
+% Setting colors along the Y axis
 w.CData = repmat(temp,1,length(w.CData))';
 
-
-
-
-
-
-cmap(1,:) = [0 0 1];   %// color first row - red
-cmap(2,:) = [1 0 0];   %// color 25th row - green
-cmap(3,:) = [0 0 1];   %// color 50th row - blue
+cmap(1,:) = [0 0 1];   %// color first row - Blue
+cmap(2,:) = [1 0 0];   %// color 25th row - Red
+cmap(3,:) = [0 0 1];   %// color 50th row - Blue
 
 [X,Y] = meshgrid([1:3],[1:50]);  %// mesh of indices
 
 cmap = interp2(X([1,25,50],:),Y([1,25,50],:),cmap,X,Y); %// interpolate colormap
-colormap(cmap)
 
-
-
-
-
-
-
-% cmap(1,:) = [0 0 1];   %// color first row - red
-% cmap(2,:) = [0.698039216 0.705882353 0];   %// color 25th row - green
-% cmap(3,:) = [1 0 0];   %// color 50th row - blue
-% cmap(4,:) = [0.698039216 0.705882353 0];   %// color 75th row - red
-% cmap(5,:) = [0 0 1];   %// color 100th row - green
-% 
+% cmap(1,:) = [0 0 1];   %// color first row - Blue
+% cmap(2,:) = [0.698039216 0.705882353 0];   %// color 25th row - Yellow
+% cmap(3,:) = [1 0 0];   %// color 50th row - Red
+% cmap(4,:) = [0.698039216 0.705882353 0];   %// color 75th row - Yellow
+% cmap(5,:) = [0 0 1];   %// color 100th row - Blue
 % 
 % [X,Y] = meshgrid([1:3],[1:100]);  %// mesh of indices
 % 
 % cmap = interp2(X([1,25,50,75,100],:),Y([1,25,50,75,100],:),cmap,X,Y); %// interpolate colormap
-% colormap(cmap)
+
+colormap(cmap)
+
 clearvars X Y cmap
 
 
@@ -115,12 +73,13 @@ clearvars X Y cmap
 % Turn the grid display off
 grid off;
 
-% Setting X and Y axis properties
+% Setting X and Y axis limits
 ax = gca;
-ax.ZLim = [0 0.0005];
-ax.XLim = [0.04 0.3];
-ax.YLim = [-60 70];
+ax.ZLim = [0 0.0005]; % Intensity Limits
+ax.XLim = [0.04 0.3]; % q value Limits
+ax.YLim = [-60 70]; % Temperature Limits
 
+% Setting Ticks and Labels on the Axes
 yticks([-60 5 70]);
 yticklabels({'-60', '70', '-60'})
 xticks(0.05:0.05:0.3);
@@ -133,7 +92,7 @@ ax.FontSize = 16;
 
 % Setting axis Labels
 % x-axis label adjusted right to match centre of axis
-% xlabel('\rm q ($\AA^{-1}$)','Interpreter','tex')%, 'Position', [1 -0.2729 -4.7617]) 
+% xlabel('q (Angstoms)')%, 'Position', [1 -0.2729 -4.7617]) 
 % y-axis label adjusted
 % ylabel('Temperature (\circC)')%,'Position',[20000 20 5]);
 % zlabel('Relative Intensity') % y-axis label
@@ -145,13 +104,16 @@ view([25 30])
 % camzoom(0.8);
 
 
-% 
-print('DHM - SAXS 2.png', '-dpng','-r600');
-print('DHM - SAXS 2.emf', '-dmeta','-r600');
-print('DHM - SAXS 2.svg', '-dsvg','-r600');
+% High resolution images 600 dpi
+
+% print('DHM - SAXS 2.png', '-dpng','-r600');
+% print('DHM - SAXS 2.emf', '-dmeta','-r600');
+% print('DHM - SAXS 2.svg', '-dsvg','-r600');
+
 
 % Saving options in order of reccomendation - Uncomment (ctrl+t/ctrl+r) to activate
-saveas(fig,'DHM - SAXS 2.fig'); % Matlab format
+
+% saveas(fig,'DHM - SAXS 2.fig'); % Matlab format
 % saveas(fig,'DHM - SAXS 1.svg'); % Scaleable vector graphic
 % saveas(fig,'DHM - SAXS 1.png'); % Portable network graphic
 % saveas(fig,'filename.bmp'); % Bitmap
